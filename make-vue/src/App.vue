2fe272c2848046dacf7818e213f41e3db59ad083
vershininyn <vershininyn@yandex.ru>
2023-08-28 20:41:06 +0700

    the intermediate commit

--- make-vue/src/App.vue
+++ make-vue/src/App.vue
@@ -1,10 +1,45 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <v-container fluid>
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+        <v-sheet class="ma-2 pa-2">
+          One of three columns
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+  </v-container>
+
+  <v-container fluid="true">
+    <v-row>
+      <v-col>
+        <v-sheet>
+          One of three columnsOne of three columnsOne of three columnsOne of three columnsOne of three columns
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container>
+  </v-container>
+
+  <v-container fluid>
+      <v-col>
+          <v-row v-for="n in 3" :key="n">
+            One of three columns
+          </v-row>
+      </v-col>
+  </v-container>
 </template>
+
+//3fr 1fr 10fr 1fr 3fr
 
 <script>
 
--- make-vue/src/main.js
+++ make-vue/src/main.js
@@ -1,9 +1,12 @@
 import './assets/main.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
